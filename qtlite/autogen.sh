3f77ad178cc42dba0503f0bd59247f1ce61eb0bb
yann <yann@29652074-91f1-0310-b939-ea11e0edb005>
2005-03-05 14:37:56 +0000

    remove qtlite to switch to Qt4

--- qtlite/autogen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd include
-find ../src | grep '\.h' | grep -v '_p\.h' | (while read file; do ln -s $file; done)
-cd private
-find ../../src | grep '_p\.h' | (while read file; do ln -s $file; done)
--- qtlite/include/qmodules.h
+++
@@ -1,5 +0,0 @@
-/* These modules are present in this configuration of Qt */
-#define QT_MODULE_TOOLS
-#define QT_MODULE_KERNEL
-#define QT_MODULE_NETWORK
-#define QT_MODULE_XML
